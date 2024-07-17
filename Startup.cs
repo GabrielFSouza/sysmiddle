240ea8478830f83212958791483bef74cde2a3b0
GabrielFSouza <58001372+GabrielFSouza@users.noreply.github.com>
2024-07-17 12:32:44 -0300

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace SysMiddle
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSingleton(new HttpClient());
+            services.AddScoped<TrelloService>();
+            services.AddScoped<AsanaService>();
+
+            
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "TrelloAsana API", Version = "v1" });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "TrelloAsana API V1");
+                c.RoutePrefix = string.Empty; 
+            });
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
